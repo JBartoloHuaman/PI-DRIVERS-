222419962359845db20b1daa39c02a42494471bc
JBartolo <jesusbartolojbh@gmail.com>
2024-01-17 00:55:05 -0500

    add css responsive at nav

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,58 +1,62 @@
-import { Link } from "react-router-dom"
-import { useState } from "react"
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { SearchDriver, refresh } from "../../redux/actions/actions";
-import logo from '../../assets/logo.svg'
-import "./Nav.css"
+import logo from "../../assets/logo.svg";
+import style from "./nav.module.css";
 
 export default function Nav() {
-    const dispatch = useDispatch()
-    const [input,setInput] = useState("");
+  const dispatch = useDispatch();
+  const [input, setInput] = useState("");
 
-    
-    const handleInput = (event) => {
-        setInput(event.target.value)
-    }
-    
-    const handleButton = (event) => {
-        event.preventDefault();
-        dispatch(SearchDriver(input))
-        document.getElementById("search").value = "";
-    }
+  const handleInput = (event) => {
+    setInput(event.target.value);
+  };
 
-    const Refresh = () => {
-        dispatch(refresh())
-    }
+  const handleButton = (event) => {
+    event.preventDefault();
+    dispatch(SearchDriver(input));
+    document.getElementById("search").value = "";
+  };
 
+  const Refresh = () => {
+    dispatch(refresh());
+  };
 
-    return (
-        <nav className="nav-cont">
-            <ul className="container">
-                <li className="li-img">
-                    <Link to="/" >
-                    <img src={logo} alt="" />
-                    </Link>               
-                </li>
-                <li>
-                    <Link onClick={Refresh} to="/home" className="home">Home</Link>
-                </li>
-                <li>
-                    <Link to="/form" className="home">Register Driver</Link>
-                </li>
-                <li className="li">
-                    <section>
-                        <form autoComplete="off">
-                            <div>
-                                <input onChange={handleInput} type="text" id="search" placeholder="Buscar..."/>
-                                    <button onClick={handleButton}>Buscar</button>
-                            </div>
-
-                        </form>
-                    </section>           
-                </li>
-            </ul>
-
-
-        </nav>
-    )
+  return (
+    <nav className={style["nav-cont"]}>
+      <ul className={style['container']}>
+        <li className={style["li-img"]}>
+          <Link to="/">
+            <img src={logo} alt="" />
+          </Link>
+        </li>
+        <li className={style["li"]}>
+          <Link onClick={Refresh} to="/home" className={style['home']}>
+            Home
+          </Link>
+        </li>
+        <li className={style["li"]}>
+          <Link to="/form" className={style['home']}>
+            Register Driver
+          </Link>
+        </li>
+        <li className={style["li-input"]}>
+          <section>
+            <form autoComplete="off">
+              <div>
+                <input
+                  onChange={handleInput}
+                  type="text"
+                  id="search"
+                  placeholder="Buscar..."
+                />
+                <button onClick={handleButton}>Buscar</button>
+              </div>
+            </form>
+          </section>
+        </li>
+      </ul>
+    </nav>
+  );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import axios from "axios";
 
-// axios.defaults.baseURL = 'http://localhost:3001'
-axios.defaults.baseURL = 'https://project-server.up.railway.app/'
+axios.defaults.baseURL = "http://localhost:3001";
+// axios.defaults.baseURL = 'https://project-server.up.railway.app/'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
-    <App />
-  </BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
- ,
-)
+);
