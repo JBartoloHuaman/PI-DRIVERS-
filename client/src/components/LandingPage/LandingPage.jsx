d1b95139bb21f1ded29fc1fb91cbbcd15171394c
JBartolo <jesusbartolojbh@gmail.com>
2024-01-22 18:58:39 -0500

    add responsive at Landingpage

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,12 +1,19 @@
-import { Link } from "react-router-dom"
-import "./landingpage.css"
+import { Link } from "react-router-dom";
+import style from "./landingpage.module.css";
+
 export default function LandingPage() {
-    return (
-        <div className="landing-cont">
-            <div className="title-cont">
-            <h1 className="title">WELCOME</h1>
-            <Link to="/home"><button>Entrar</button></Link>
-            </div>
-        </div>
-    )
+  return (
+    <div className={style['landing-cont']}>
+      <div className={style['title-cont']}>
+        <h1 className={style['title']}>WELCOME</h1>
+        <Link to="/home">
+          <button className={style["button-landing"]}>Entrar
+          <span></span>
+          <span></span>
+          <span></span>
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
